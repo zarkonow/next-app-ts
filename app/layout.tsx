a947b4f3e66c5e3fd4e1ccd5774bcf247d9d5c94
zarkonow <stojanov1987@gmail.com>
2025-02-13 18:18:45 +0100

    Add NavBar to layout, update Home page structure, and create Dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>NavBar</div>
         {children}
       </body>
     </html>
